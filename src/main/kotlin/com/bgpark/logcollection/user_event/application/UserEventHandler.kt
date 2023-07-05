cfd9dbb85025237e8c6bffe18a9816ff20202bc5
bgpark <bgpark82@gmail.com>
2023-07-05 15:40:00 +0900

    feat: UserAgent 값 payload에 추가

--- src/main/kotlin/com/bgpark/logcollection/user_event/application/UserEventHandler.kt
+++ src/main/kotlin/com/bgpark/logcollection/user_event/application/UserEventHandler.kt
@@ -13,10 +13,10 @@ class UserEventHandler(
     private val messageProducer: MessageProducer
 ) {
     suspend fun create(request: ServerRequest): ServerResponse {
-        val userEvent = request.awaitBody<UserEvent>()
+        val message = attachExtraInfo(request)
 
         CoroutineScope(Dispatchers.IO).launch {
-            messageProducer.send("TOPIC", userEvent.uuid)
+            messageProducer.send("TOPIC", message.toString())
         }
 
         return ServerResponse.ok().json().bodyValueAndAwait("done")
@@ -24,5 +24,42 @@ class UserEventHandler(
 
     suspend fun createBulk(request: ServerRequest): ServerResponse {
         return ServerResponse.ok().json().bodyValueAndAwait("TODO")
+    }
+
+
+    /**
+     * http header에서 필요한 정보를 추출하여 payload에 추가
+     *
+     * @param request http 요청
+     * @return payload
+     */
+    private suspend fun attachExtraInfo(request: ServerRequest): Map<String, Any> {
+        val event = request.awaitBody<UserEvent>()
+        println("request : ${event}")
+        println("request body : ${event.data}")
+
+        val result = mutableMapOf<String, Any>()
+        result.putAll(event.data)
+
+        addUserAgentHeader(request, result)
+        return result
+    }
+
+    /**
+     * User-Agent 헤더값을 결과로 반환
+     * User-Agent : 요청을 보낸 브라우저의 종류와 버전 확인
+     * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/User-Agent
+     */
+    private fun addUserAgentHeader(
+        request: ServerRequest,
+        result: MutableMap<String, Any>
+    ) {
+        val userAgentHeader = request.headers().header("User-Agent")
+
+        result["userAgent"] =
+            when (userAgentHeader.size) {
+                1 -> userAgentHeader[0]
+                else -> "Unknown"
+            }
     }
 }
--- src/main/kotlin/com/bgpark/logcollection/user_event/dto/UserEvent.kt
+++ src/main/kotlin/com/bgpark/logcollection/user_event/dto/UserEvent.kt
@@ -3,7 +3,7 @@ package com.bgpark.logcollection.user_event.dto
 import com.fasterxml.jackson.annotation.JsonProperty
 
 data class UserEvent(
-    @JsonProperty("uuid") val uuid: String
+    @JsonProperty("data") val data: Map<String, Any>
 ) {
 
 }
