844dbcad9fdbdc6f47305d77df11d8eb9c83c6d9
bgpark <bgpark82@gmail.com>
2023-07-19 14:43:11 +0900

    chore: 포트 확인 스크립트 제거

--- script/code-deploy/application_start.sh
+++ script/code-deploy/application_start.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
 
-SEVER_PORT=8080
-
 cd /home/ubuntu/build
 nohup java -jar log-collector.jar 1>/dev/null 2>&1 &
-
-CONTINUE=1
-while [ ${CONTINUE} -eq 1 ]
-do
-    sleep 1
-    PORT_STATUS=`netstat -an | grep LISTEN | grep ":${SERVER_PORT}" | wc -l`
-    if [ ${PORT_STATUS} -eq 1 ]
-    then
-      CONTINUE=0
-    fi
-done
